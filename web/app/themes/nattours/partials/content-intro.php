ac7ccfe07c6bc84c558a68a85cec9f07082e2420
Elias Ylänen <elias.ylanen@gmail.com>
2017-03-02 16:21:47 +0200

    Restructuring; fixing formtting fuckups

--- web/app/themes/nattours/partials/content-intro.php
+++ web/app/themes/nattours/partials/content-intro.php
@@ -1,20 +1,19 @@
 <style>
-	.header--location {
-		background-image: 
-		-webkit-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
-		url("<?php the_field('introduction_image'); ?>");
-		background-image: 
-		-moz-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
-		url("<?php the_field('introduction_image'); ?>");
-		background-image: 
-		-o-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
-		url("<?php the_field('introduction_image'); ?>");
-		background-image: 
-		linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
-		url("<?php the_field('introduction_image'); ?>"); }
+  .header--location {
+    background-image: 
+      -webkit-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
+      -moz-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
+      -o-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
+      linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
+      url("<?php the_field('introduction_image'); ?>");
+    }
 </style>
 
-<?php 
-echo '<h4>' . pll__( 'Header for introduction text' ) . '</h4>';
-the_content();
-?>
+<article class="location__intro">
+  <section class="location__intro__content">
+    <div class="location__intro__content__text"> 
+      <h4><?= pll__( 'Header for introduction text' ) ?></h4>
+      <?php get_first_paragraph( the_content() ); ?>
+    </div>
+  </section>
+</article>
