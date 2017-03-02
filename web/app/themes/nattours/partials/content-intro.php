e47c3bf522ca595d361f7e735bba9e78a306e006
Elias Ylänen <elias.ylanen@gmail.com>
2017-03-02 16:45:55 +0200

    Examine viability of current location intro gallery execution

--- web/app/themes/nattours/partials/content-intro.php
+++ web/app/themes/nattours/partials/content-intro.php
@@ -1,9 +1,6 @@
 <style>
   .header--location {
-    background-image: 
-      -webkit-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
-      -moz-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
-      -o-linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
+    background-image:
       linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),
       url("<?php the_field('introduction_image'); ?>");
     }
@@ -15,5 +12,6 @@
       <h4><?= pll__( 'Header for introduction text' ) ?></h4>
       <?php get_first_paragraph( the_content() ); ?>
     </div>
+    <?php print_r( get_field('introduction_gallery' ) ); ?>
   </section>
 </article>
