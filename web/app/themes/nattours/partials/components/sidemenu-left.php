d68cefdf1ccf6249bbd7e2beaf15da4eda28f414
Elias Ylänen <elias.ylanen@gmail.com>
2017-06-14 12:57:09 +0300

    Language switcher in left menu

--- web/app/themes/nattours/partials/components/sidemenu-left.php
+++ web/app/themes/nattours/partials/components/sidemenu-left.php
@@ -12,20 +12,23 @@ $sidemenu_locations = new WP_Query( [
       <i class="fa fa-times close-modal" id="leftClose" aria-hidden="false"></i>
     </div>
   </div>
-  <div class="content--left">
-    <h6 class="sidemenu__location"><?= pll__('Helsinki') ?></h6><a href="/" class="sidemenu__all"><?= pll__('All locations') ?></a>
-    <div class="graphic-content visible-xs" id="locationGallery">
-      <?php if ( $sidemenu_locations->have_posts() ) : while ( $sidemenu_locations->have_posts() ) : $sidemenu_locations->the_post();
-        get_template_part( 'partials/content', 'excerpt-sidemenu' );
-      endwhile; endif; ?>
+  <div class="container-fluid">
+    <div class="content--left">
+      <h6 class="sidemenu__location"><?= pll__('Helsinki') ?></h6><a href="/" class="sidemenu__all"><?= pll__('All locations') ?></a>
+      <div class="graphic-content visible-xs" id="locationGallery">
+        <?php if ( $sidemenu_locations->have_posts() ) : while ( $sidemenu_locations->have_posts() ) : $sidemenu_locations->the_post();
+          get_template_part( 'partials/content', 'excerpt-sidemenu' );
+        endwhile; endif; ?>
+      </div>
+      <div class="graphic-content hidden-xs">
+        <?php if ( $sidemenu_locations->have_posts() ) : while ( $sidemenu_locations->have_posts() ) : $sidemenu_locations->the_post();
+          get_template_part( 'partials/content', 'excerpt-sidemenu' );
+        endwhile; endif; wp_reset_postdata(); ?>
+      </div>
     </div>
-    <div class="graphic-content hidden-xs">
-      <?php if ( $sidemenu_locations->have_posts() ) : while ( $sidemenu_locations->have_posts() ) : $sidemenu_locations->the_post();
-        get_template_part( 'partials/content', 'excerpt-sidemenu' );
-      endwhile; endif; wp_reset_postdata(); ?>
+    <div class="content--right">
+      <span> <?= pll__('Language') ?> </span>
+      <?php nattours_main_menu(); ?>
     </div>
-  </div>
-  <div class="content--right">
-    <span> <?= pll__('Language') ?> </span>
   </div>
 </section>
