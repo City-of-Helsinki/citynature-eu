b8623d09d4d4963a34b50fe559b5408b99b8bf3f
Timo Leppanen <timo@luuptek.fi>
2018-01-05 09:20:15 +0200

    adding footer menu

--- web/app/themes/nattours/library/widgets/nav-menus.php
+++ web/app/themes/nattours/library/widgets/nav-menus.php
@@ -10,28 +10,51 @@
  * Main menu
  */
 function nattours_main_menu() {
-    wp_nav_menu( [
-        'theme_location'  => 'top_nav',
-        'container'       => false,
-        'container_class' => '',
-        'container_id'    => '',
-        'menu_class'      => 'menu',
-        'menu_id'         => '',
-        'echo'            => true,
-        'fallback_cb'     => '',
-        'before'          => '',
-        'after'           => '',
-        'link_before'     => '',
-        'link_after'      => '',
-        'items_wrap'      => '%3$s',
-        'depth'           => 4,
-        'walker'          => new Nord\WP_navwalker
-    ] );
+	wp_nav_menu( [
+		'theme_location'  => 'top_nav',
+		'container'       => false,
+		'container_class' => '',
+		'container_id'    => '',
+		'menu_class'      => 'menu',
+		'menu_id'         => '',
+		'echo'            => true,
+		'fallback_cb'     => '',
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
+		'items_wrap'      => '%3$s',
+		'depth'           => 4,
+		'walker'          => new Nord\WP_navwalker
+	] );
+}
+
+/**
+ * Main menu
+ */
+function nattours_footer_menu() {
+	wp_nav_menu( [
+		'theme_location'  => 'footer_nav',
+		'container'       => false,
+		'container_class' => '',
+		'container_id'    => '',
+		'menu_class'      => 'menu',
+		'menu_id'         => '',
+		'echo'            => true,
+		'fallback_cb'     => '',
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
+		'items_wrap'      => '%3$s',
+		'depth'           => 1,
+	] );
 }
 
 /**
  * Register the menu
  */
 register_nav_menus( [
-    'top_nav' => __( 'Main menu', TEXT_DOMAIN )
+	'top_nav'    => __( 'Main menu', TEXT_DOMAIN ),
+	'footer_nav' => __( 'Footer menu', TEXT_DOMAIN ),
 ] );
--- web/app/themes/nattours/templates/footer.php
+++ web/app/themes/nattours/templates/footer.php
@@ -7,14 +7,22 @@
  */
 
 ?>
-
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="footer-menu-column">
+                <ul class="footer-links-menu">
+	                <?php nattours_footer_menu() ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?>
-  <footer class="footer">
-    <span>Footer</span>
-  </footer>
-  <!-- <script src="http://code.responsivevoice.org/responsivevoice.js"></script> -->
-  <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
-<link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css' rel='stylesheet' />
+<!-- <script src="http://code.responsivevoice.org/responsivevoice.js"></script> -->
+<script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
+<link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css'
+      rel='stylesheet'/>
 <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.62.0/dist/L.Control.Locate.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.62.0/dist/L.Control.Locate.min.css">
 </body>
