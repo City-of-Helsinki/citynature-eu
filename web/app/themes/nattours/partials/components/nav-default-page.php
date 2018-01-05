8915a029ad264f38ac9049b1244e097ef6ea31eb
Timo Leppanen <timo@luuptek.fi>
2018-01-05 10:56:41 +0200

    default page template

---
+++ web/app/themes/nattours/partials/components/nav-default-page.php
@@ -0,0 +1,11 @@
+<div class="header__nav">
+  <div class="header__nav__sidemenu">
+
+  </div>
+  <a href="<?php echo home_url('/') ?>" class="header__nav__link">
+    <span>citynature.eu</span>
+  </a>
+  <span class="header__nav__map">
+
+  </span>
+</div>
--- web/app/themes/nattours/partials/content-page.php
+++ web/app/themes/nattours/partials/content-page.php
@@ -7,9 +7,43 @@
  *
  */
 
+get_header();
 ?>
 
-<article <?php post_class( 'post-container post-' . sanitize_title( get_the_title() ) ); ?>>
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-</article>
+<header class="header header--location" id="locationHeader">
+    <!--<div class="header__overlay"></div>-->
+	<?php get_template_part( 'partials/components/nav-default-page' ); ?>
+    <div class="header__texts">
+        <h3><?php the_title(); ?></h3>
+    </div>
+    <style>
+        .header--location {
+            background-image: linear-gradient(
+                    to bottom,
+                    rgba(0, 0, 0, 0.5),
+                    rgba(0, 0, 0, 0.1) 10%,
+                    rgba(0, 0, 0, 0.1) 70%,
+                    rgba(0, 0, 0, 0.6)
+            ),
+            url("<?php echo get_the_post_thumbnail_url(); ?>");
+        }
+    </style>
+
+</header>
+<?php if ( wp_get_img_caption( get_post_thumbnail_id() ) ): ?>
+    <span class="img-caption"><?= wp_get_img_caption( get_post_thumbnail_id() ); ?></span>
+<?php endif; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="default-page-column">
+            <article>
+		        <?php the_content(); ?>
+            </article>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
