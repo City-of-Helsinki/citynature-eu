d02e6ec9e86cbedb2703df57dd002d3891cd1f23
Timo Leppanen <timo@luuptek.fi>
2018-01-23 10:34:36 +0200

    add more translations

--- web/app/themes/nattours/functions.php
+++ web/app/themes/nattours/functions.php
@@ -375,4 +375,5 @@ if(function_exists('pll_register_string')) {
     pll_register_string( 'Read more about species', 'Read more about species' );
     pll_register_string( 'Read more', 'Read more' );
 	pll_register_string( 'Front page footer text', 'Front page footer text' );
+	pll_register_string( 'Switch language', 'Switch language' );
 }
